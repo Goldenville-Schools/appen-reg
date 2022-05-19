6df24e8419eca39aa6a80f237cf56d4f51be9ab6
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-05-20 00:02:05 +0100

    updated

---
+++ src/components/pages/Individual/SingleSignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleSignUp = () => {
+  return (
+    <div>
+      <h1>REGISTER AS INDIVIDUAL</h1>
+    </div>
+  )
+}
+
+export default SingleSignUp
--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div className='register-container'>
-      <h1>Hell Register Here</h1>
+    <div className='reg'>
+      <div className='reg-wrapper'>
+          <div className='reg-box1'>
+            <h3>REGISTER AS <span>GROUP</span></h3>
+            <Link to='./SchoolSignUp'><button>register</button></Link>
+          </div>
+          <div className='reg-box2'>
+            <h3>REGISTER AS <span>INDIVIDUAL</span></h3>
+            <Link to='./SingleSignUp'><button>register</button></Link>
+          </div>
+      </div>
+
     </div>
   )
 }
