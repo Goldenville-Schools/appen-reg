23f629e62e0d6fad3123afd642931c6146591d3b
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-06-16 16:07:06 +0100

    updated

--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -10,14 +10,21 @@ const Register = () => {
             <Navreg />
           <div className='reg-wrapper'>
               <div className='reg-box1'>
-                <h3>REGISTER AS <span>GROUP / ORGANIZATION</span></h3>
-                <Link to='/GroupOnline'><button className='online-btn'>Online</button></Link>
-                <Link to='/GroupPhy'><button type='button' className='physical-btn'>Physical</button></Link>
+                <h3>REGISTER AS <span>GROUP/ORGANIZATION</span></h3>
+                  <div className='btn-flex'>
+                  <Link to='/GroupOnline'><button className='online-btn'>Online</button></Link>
+                  <Link to='/GroupPhy'><button type='button' className='physical-btn'>Physical</button></Link>
+                  </div>
+        
+                
+              
               </div>
               <div className='reg-box2'>
                 <h3>REGISTER AS <span>INDIVIDUAL</span></h3>
-                <Link to='/IndiviOnline'><button type='button' className='online2-btn'>Online</button></Link>
-                <Link to='/IndiviPhy'><button type='button' className='physical2-btn'>Physical</button></Link>
+                <div className='btn2-flex'>
+                  <Link to='/IndiviOnline'><button type='button' className='online2-btn'>Online</button></Link>
+                  <Link to='/IndiviPhy'><button type='button' className='physical2-btn'>Physical</button></Link>
+                </div>
               </div>
           </div>
         </div>
