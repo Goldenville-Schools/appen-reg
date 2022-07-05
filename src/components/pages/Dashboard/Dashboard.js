4026aa8d422f0f9588bfcb69ea9231098c643dec
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-07-05 14:13:34 +0100

    updated

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -2,9 +2,7 @@ import React from 'react'
 import NavbarDash from '../../DashboardNav/NavbarDash'
 import Sidebar from '../../Dashboard_sidebar/Sidebar'
 import '../Dashboard/Dashboard.css'
-import img2 from '../../assets/img4.jpg'
-import img3 from '../../assets/img7.jpg'
-import Carousel from 'react-bootstrap/Carousel'
+import { AiFillCheckSquare } from "react-icons/ai";
 
 
 
@@ -16,48 +14,89 @@ const Dashboard = () => {
      <div className='dashboard_container'>
       <NavbarDash/>
         <section className='hero_main container-fluid'>
-          <div className='container-fluid'>
-            <div className='hero_bg'>
-            <Carousel>
-              <Carousel.Item interval={1000}>
-                <img
-                  className="d-block w-100"
-                  src={img2}
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item interval={500}>
-                <img
-                  className="d-block w-100"
-                  src={img2}
-                  alt="Second slide"
-                />
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={img2}
-                  alt="Third slide"
-                />
-                <div>
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-                </div>
-                
-              </Carousel.Item>
-            </Carousel>
+          <div className='container' id='hero_bg'>
+            <div className='hero_wrapper'>
+              <h1>12TH APEN Conference 2022</h1>
+              <h3>Date -27th – 28th October, 2022 | Conference Theme - Emerge: Reimagine, Redefine, Reposition</h3>
+              
+              <div className='btn_wrapper'>
+                <button href='#' className='btn_hero'>See More</button>
+              </div>  
           </div>
         </div>
+        <section className='topics mt-4'>
+          <div className='container'>
+              <div className='row'>
+              <h3>POSSIBLE TITLES/TOPIC</h3>
+              <div className='liner'></div>
+                  <div className='col'>    
+                    <div className='contents'>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>How to deal with bullies/how to handle the parents of bullies</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Safeguarding</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Gamification in the classroom</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>AI, VR, Robotics and Drones</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span> Use of Social Media in Schools</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Coding/Programming</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Assessment of the Future – 21C Assessment</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col'>
+                  <div className='contents'>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>he Utility of a Common Entrance Exam for International Schools</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>SEAL</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Quadrants and Quotients: IQ, EQ, SQ and AQ</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Beating their Own Path – Prepare your students for the road, 
+                          don’t prepare the road for children</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Developing a Coaching Culture</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Digital Marketing for School</span>
+                      </div>
+                      <div className='content_item'>
+                        <AiFillCheckSquare className='icon'/> 
+                        <span>Environmental Awareness – Denial and Acceptance</span>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+        </section>
       </section> 
      </div>   
     </div>
