b4022604cd76ef2f6175fc79a1460a41f3774e6c
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-07-08 14:40:50 +0100

    update

--- src/components/Dashboard_sidebar/Sidebar.js
+++ src/components/Dashboard_sidebar/Sidebar.js
@@ -48,7 +48,7 @@ const Sidebar = () => {
     <div className='sidebars'>
          <div style={{width: isOpen ? "250px" : "50px"}} className="sidebar">
                <div className="top_section">
-                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo"><img src={logo}  alt="logo"/></h1>
+                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo"><Link to='/Dashboard'><img src={logo}  alt="logo"/></Link></h1>
                    <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
                        <FaBars onClick={toggle}/>
                    </div>
