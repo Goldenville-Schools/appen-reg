338346ff724a189d020b6d1e22046cc5cb0d2ff3
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-07-26 16:12:45 +0100

    updated

--- src/components/Dashboard_sidebar/Sidebar.js
+++ src/components/Dashboard_sidebar/Sidebar.js
@@ -31,7 +31,7 @@ const Sidebar = () => {
     //   },
       {
           path:"/Signin",
-          name:"LogOut",
+          name:"Logout",
           icon:<RiLogoutCircleRFill/>
       }
    
@@ -40,7 +40,7 @@ const Sidebar = () => {
   
   return (
     <div className='sidebars'>
-        <div style={{width: isOpen ? "250px" : "55px"}} className="sidebar">
+        <div style={{width: isOpen ? "220px" : "50px"}} className="sidebar">
                <div className="top_section">
                    <h1 style={{display: isOpen ? "block" : "none"}} className="logo"><Link to='/Dashboard'><img src={logo}  alt="logo"/></Link></h1>
                    <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
