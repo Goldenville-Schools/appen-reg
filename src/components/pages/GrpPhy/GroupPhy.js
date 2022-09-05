1e5a667885088962398635497334cac5db3d4ee4
clandestine33 <ed.oguamanam@gmail.com>
2022-09-05 18:17:45 +0100

    modified payment page

--- src/components/pages/GrpPhy/GroupPhy.js
+++ src/components/pages/GrpPhy/GroupPhy.js
@@ -105,7 +105,7 @@ const handleSubmit =(e)=>{
     });
 
   }else{
-    window.location = `${process.env.REACT_APP_PAYSTACK_URL}?amount=${amount}&readonly=amount`
+    window.location.href = `${process.env.REACT_APP_PAYSTACK_URL}?amount=${amount}&readonly=amount`
   }
 
 }
