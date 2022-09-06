22d39931527ebca0962eaee223d59805b6458a8f
clandestine33 <ed.oguamanam@gmail.com>
2022-09-06 10:57:41 +0100

    fixed group online price

--- src/components/pages/GrpOnline/GroupOnline.js
+++ src/components/pages/GrpOnline/GroupOnline.js
@@ -98,7 +98,7 @@ const handleAttendeeChange =(e, index)=>{
 const handleSubmit=(e)=>{
   e.preventDefault();
   if (attendeesForm.length === 5 || attendeesForm.length < 5){
-    const amount = calcAmount(groupFormField, attendeesForm, 190000)
+    const amount = calcAmount(groupFormField, attendeesForm, 180000)
     console.log(amount)
     window.location = "/Payment"
   }else{
