4099a88108f5da87730fb7bae932931ce499b7bd
clandestine33 <ed.oguamanam@gmail.com>
2022-09-06 07:27:36 +0100

    bug fix

--- src/components/pages/IndiviPhysical/IndiviPhy.js
+++ src/components/pages/IndiviPhysical/IndiviPhy.js
@@ -63,6 +63,7 @@ const IndiviPhy = () => {
   const amount = calcAmount(formField, [], 72000)
   console.log(amount);
   // localStorage.setItem('IndividualPhysicalDetails', JSON.stringify([{...formField}]))
+  console.log(process.env.REACT_APP_PAYSTACK_URL);
   window.location = `${process.env.REACT_APP_PAYSTACK_URL}?amount=${amount}&readonly=amount` 
 }
 
